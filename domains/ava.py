d96646c75089fe29fe16e95402c1c9334215ca41
Checksum <collier.bell20@gmail.com>
2021-06-21 22:11:00 -0500

    possible banner support soon :package:

--- domains/ava.py
+++ domains/ava.py
@@ -15,7 +15,35 @@ BASE_DOMAIN = glob.config.domain
 domain = Domain({f'a.{BASE_DOMAIN}', 'a.ppy.sh'})
 
 AVATARS_PATH = Path.cwd() / '.data/avatars'
+BANNERS_PATH = Path.cwd() / '.data/banners'
+DEFAULT_BANNER = BANNERS_PATH / 'default.jpg'
 DEFAULT_AVATAR = AVATARS_PATH / 'default.jpg'
+
+@domain.route(re.compile(r'^/banners/(?:\d{1,10}(?:\.(?:jpg|jpeg|png))?|favicon\.ico)?$'))
+async def get_banner(conn: Connection) -> Optional[bytes]:
+    filename = conn.path[1:]
+
+    if '.' in filename:
+        # user id & file extension provided
+        path = BANNERS_PATH / filename
+        if not path.exists():
+            path = DEFAULT_BANNER
+    elif filename not in ('', 'favicon.ico'):
+        # user id provided - determine file extension
+        for ext in ('jpg', 'jpeg', 'png'):
+            path = BANNERS_PATH / f'{filename}.{ext}'
+            if path.exists():
+                break
+        else:
+            # no file exists
+            path = BANNERS_PATH
+    else:
+        # empty path or favicon, serve default avatar
+        path = BANNERS_PATH
+
+    ext = 'png' if path.suffix == '.png' else 'jpeg'
+    conn.resp_headers['Content-Type'] = f'image/{ext}'
+    return path.read_bytes()
 
 
 @domain.route(re.compile(r'^/(?:\d{1,10}(?:\.(?:jpg|jpeg|png))?|favicon\.ico)?$'))
